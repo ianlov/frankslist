f8e755fee20864914e5352cd6c8737458d5f28b6
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-11-15 06:42:38 -0800

    revert

--- seed/data.js
+++ seed/data.js
@@ -1,48 +1,45 @@
-import db from '../db/connection.js'
-import Hobby from '../models/hobby.js'
-import User from '../models/user.js'
-import bcrypt from 'bcrypt'
+import db from "../db/connection.js";
+import Hobby from "../models/hobby.js";
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 const insertData = async () => {
-
-  await db.dropDatabase()
+  await db.dropDatabase();
 
   const user1 = new User({
-    username: 'ian',
-    email: 'root@super.gmail.com',
-    password_digest: await bcrypt.hash('!a$ecureP@ssw0Rd55!', 11)
-  })
-  await user1.save()
+    username: "ian",
+    email: "root@super.gmail.com",
+    password_digest: await bcrypt.hash("!a$ecureP@ssw0Rd55!", 11),
+  });
+  await user1.save();
 
   const user2 = new User({
-    username: 'bianca',
-    email: 'b.anca@super.gmail.com',
-    password_digest: await bcrypt.hash('!$h0pp3R1', 11)
-  })
-  await user2.save()
+    username: "bianca",
+    email: "b.anca@super.gmail.com",
+    password_digest: await bcrypt.hash("!$h0pp3R1", 11),
+  });
+  await user2.save();
 
   const user3 = new User({
-    username: 'enzo',
-    email: 'n.zo@super.gmail.com',
-    password_digest: await bcrypt.hash('!$eller4Lif3', 11)
-  })
-  await user3.save()
+    username: "enzo",
+    email: "n.zo@super.gmail.com",
+    password_digest: await bcrypt.hash("!$eller4Lif3", 11),
+  });
+  await user3.save();
 
   const user4 = new User({
-    username: 'kumi',
-    email: 'kumi@super.gmail.com',
-    password_digest: await bcrypt.hash('L0v32!p4int', 11)
-  })
-  await user4.save()
+    username: "kumi",
+    email: "kumi@super.gmail.com",
+    password_digest: await bcrypt.hash("L0v32!p4int", 11),
+  });
+  await user4.save();
 
-  const hobbies = [
-   
-  ]
+  const hobbies = [];
 
-  await Hobby.insertMany(hobbies)
-  console.log('Created users & hobbies!')
+  await Hobby.insertMany(hobbies);
+  console.log("Created users & hobbies!");
 
-  db.close()
-}
+  db.close();
+};
 
-insertData()
+insertData();
