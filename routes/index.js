86889aa780533631fa37b885f2a2464f3d1bf2ff
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-11-15 09:02:11 -0800

    seeded

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-import { Router } from 'express'
-import hobbiesRoutes from './hobbies.js'
-import usersRoutes from './user.js'
+import { Router } from "express";
+import hobbiesRoutes from "./hobbies.js";
+import usersRoutes from "./users.js";
 
-const router = Router()
+const router = Router();
 
-router.get('/', (req, res) => res.send('This is the api root!'))
+router.get("/", (req, res) => res.send("This is the api root!"));
 
-router.use('/', usersRoutes)
-router.use('/', hobbiesRoutes)
+router.use("/", usersRoutes);
+router.use("/", hobbiesRoutes);
 
-export default router
+export default router;
