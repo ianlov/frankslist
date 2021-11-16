cb46cf779dd968918d5fd2a9189c6f837821ff60
Ian Lovice <ianlovice@Ians-Air.attlocal.net>
2021-11-16 15:09:38 -0500

    working on related component

--- client/src/components/Related/Related.jsx
+++ client/src/components/Related/Related.jsx
@@ -1,15 +1,27 @@
 import related from "../../utilities/related.js";
 import "./style.css";
 
-const Related = () => {
-  
+import { Link } from "react-router-dom";
+import { useParams } from "react-router";
+
+const Related = (props) => {
+  const relatedArray = related(props.hobbyDetail)
 
   return (
     <div className="related-container">
       <div className="related-container__title">Related</div>
-      related.
+      {relatedArray.map((hobby, idx) => {
+        <Link 
+          className="related-container__related"
+          to={`/hobbies/${hobby.id}`}
+          key={idx}
+        >
+          <img src={hobby.img_url} alt={`Related ${idx}`} />
+          <p>{hobby.name}</p>
+        </Link>
+      })}
     </div>
   )
 }
 
-export default Related
+export default Related;
