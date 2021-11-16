cc22e3f513c97f88c86c2a80807e8c0e43714bc8
Ian Lovice <ianlovice@Ians-Air.attlocal.net>
2021-11-16 14:48:16 -0500

    working on related

--- client/src/components/Related/Related.jsx
+++ client/src/components/Related/Related.jsx
@@ -2,9 +2,12 @@ import related from "../../utilities/related.js";
 import "./style.css";
 
 const Related = () => {
+  
+
   return (
     <div className="related-container">
-      
+      <div className="related-container__title">Related</div>
+      related.
     </div>
   )
 }
