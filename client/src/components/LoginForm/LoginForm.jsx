e49b3b55b2e9529a33588bbb851b3709699d79be
Ricky Boyd <rickyboyd23@gmail.com>
2021-11-17 17:49:48 -0500

    login, signup, create

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,34 +1,94 @@
 import './LoginForm.css'
+import { useState } from 'react'
+import { signIn } from '../../services/users'
+import { useHistory } from 'react-router-dom'
 
-const LogIn = () => {
+const LogIn = (props) => {
+  const history = useHistory()
+
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    isError: false,
+    errorMsg: '',
+  })
+
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const onSignIn = async (event) => {
+    event.preventDefault()
+    const { setUser } = props
+    try {
+      const user = await signIn(form)
+      setUser(user)
+      history.push('/')
+    } catch (error) {
+      console.error(error)
+      setForm({
+        isError: true,
+        errorMsg: 'Invalid Credentials',
+        email: '',
+        password: '',
+      })
+    }
+  }
+
+  const renderError = () => {
+    const toggleForm = form.isError ? 'danger' : ''
+    if (form.isError) {
+      return (
+        <button className="btn__2" type='submit' className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      )
+    } else {
+      return <button className="btn__2" type='submit'>Member Login</button>
+    }
+  }
+
+  const { email, password } = form
   
   return (
 <>
   <div className="box">
     <section className="container">
       <div className="header">Login</div>
-          <form className="form">
+          <form onSubmit={onSignIn} className="form">
+
             <div className="username">
-              <h5 className="text__signin__1">Username</h5>
-                <label>
-                  <input type="text" username="username" />
-                </label>
+                <label className="text__signin__1">Email</label>
+              <input
+                required
+                  type="text"
+                  name="email"
+                  value={email}
+                  placeholer="Type your email"
+                  onChange={handleChange}
+                />
             </div>
 
             <div className="password">
-              <h5 className="text__signin__2">Password</h5>
-                <label >
-                  <input type="text" password="password" />
-                </label>
-            </div>
-
-            <h6 classname="forgot">Forgot Password?</h6>
-
-            <div>
-              <button className="btn__2" type="submit">Member Login</button>
+                <label className="text__signin__2">Password</label>
+              <input
+                required
+                name="password"
+                value={password}
+                type="text"
+                placeholer="Type your password"
+                onChange={handleChange}
+              />
+              
+              <h6 classname="forgot">Forgot Password?</h6>
+              
+              {renderError()}
             </div>
           </form>
-
+          
             <h6>Not a member yet?</h6>
             
             <div>
