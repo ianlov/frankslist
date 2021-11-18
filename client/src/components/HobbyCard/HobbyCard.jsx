49761953318049f2cdc78ec5fea483c67194d675
Ian Lovice <ianlovice@Ians-Air.attlocal.net>
2021-11-18 17:32:25 -0500

    showing all hobbies no fuctionality

--- client/src/components/HobbyCard/HobbyCard.jsx
+++ client/src/components/HobbyCard/HobbyCard.jsx
@@ -1,48 +1,65 @@
-import "./HobbyCard.css";
-const HobbyCard = () => {
-  return (
-    <>
-      <div className="results">
-        <h5> SEARCH RESULTS FOR THIS THING YOU SEARCHED</h5>
-      </div>
-      <div className="cards">
-        <div className="card">
-          <div className="hobbytest">
-            <img
-              className="hobby__pic"
-              src="https://i.imgur.com/BHeG8or.jpg?1"
-            />
-            <button className="dummy">DUMMY CLICK</button>
-          </div>
-          <div className="hobbyinfo">
-            <div className="header">
-              <div className="activity">
-                <h1>MIKE NEZZER MIKE NEZZER</h1>
-              </div>
-              <h5>1 2 3 4 5</h5>
-              <p>Details</p>
-            </div>
-            <div className="details">
-              <div className="left__side">
-                <div className="category">
-                  <h6>LOCATION</h6>
-                  <h6>PRICE</h6>
-                  <h6>RISK</h6>
-                </div>
-              </div>
+import "./style.css";
+const HobbyCard = ({ hobby }) => {
+  const handlePrice = (price) => {
+    if (price < 20) {
+      return "$";
+    } else if (price >= 20 && price < 100) {
+      return "$$";
+    } else if (price >= 100 && price < 5000) {
+      return "$$$";
+    } else {
+      return "$$$$";
+    }
+  }
 
-              <div className="right__side">
-                <div className="values">
-                  <h6>THE STREETS</h6>
-                  <h6>$32.17</h6>
-                  <h6>BAD MOFO</h6>
-                </div>
-              </div>
-            </div>
+  const handleRisk = (risk) => {
+    let message = "";
+    switch (risk) {
+      case 1:
+        message = "No Risk";
+        break;
+      case 2:
+        message = "Little Risk";
+        break;
+      case 3:
+        message = "Moderate Risk";
+        break;
+      case 4:
+        message = "Risky";
+        break;
+      case 5:
+        message = "Dangerous";
+        break;
+      default:
+        message = "N/A"
+        break;
+    }
+    return message
+  }
+
+  return (
+    <div className="hobby-card" >
+      <div className="hobby-card__image-box">
+        <img src={hobby.img_url} alt={hobby.name} />
+        <button>Save Hobby</button>
+      </div>
+      <div className="hobby-card__info" >
+        <h1>{hobby.name}</h1>
+        <p>{hobby.rating ? hobby.rating : "N/A"}</p>
+        <div className="hobby-card__info__details" >
+          <div className="hobby-card__info__details__left" >
+            <p>Indoors</p>
+            <p>Price</p>
+            <p>Risk</p>
+          </div>
+          <div className="hobby-card__info__details__right" >
+            <p>{hobby.indoors ? "Indoors" : "Outdoors"}</p>
+            <p>{handlePrice(hobby.price.low)}</p>
+            <p>{handleRisk(hobby.risk)}</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
