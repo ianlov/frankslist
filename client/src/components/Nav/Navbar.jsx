15646f53f649e914e69e733a650935ed45e8e767
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-11-18 09:40:43 -0800

    nav styling and hamburgermenu

--- client/src/components/Nav/Navbar.jsx
+++ client/src/components/Nav/Navbar.jsx
@@ -1,52 +1,88 @@
 import "./navbar.css";
 import Search from "../Search/Search";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [hamDisplay, setHamDisplay] = useState(false);
+
   return (
-    <nav className="navcontainer">
-      <div className="nav__left">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <div className="nav__logo">
-            <img
-              className="nav__logo__img"
-              src="https://images.unsplash.com/photo-1624300603538-1207400f4116?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
-              alt=""
-            />
-            <h1>frankslist</h1>
-          </div>
-        </Link>
-        <Search />
-      </div>
-      <div className="nav__right">
-        <ul className="nav__ul">
+    <>
+      <nav className="navcontainer">
+        <div className="nav__left">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div className="nav__logo">
+              <img
+                className="nav__logo__img"
+                src="https://i.imgur.com/TEHnXjq.jpg"
+                alt=""
+              />
+              <h1 style={{ marginLeft: "-21px" }}>frankslist</h1>
+            </div>
+          </Link>
+          <Search />
+        </div>
+        <div className="nav__right">
+          <ul className="nav__ul">
+            <li>
+              <Link to="/hobbies">
+                <a>All Hobbies</a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/aboutus">
+                <a>About Us</a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/sign-in">
+                <a>Log in</a>
+              </Link>
+            </li>
+          </ul>
+          <Link to="/sign-in">
+            <button className="nav__button">Sign Up</button>
+          </Link>
+        </div>
+        <div className="hamburger" onClick={() => setHamDisplay(!hamDisplay)}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </nav>
+      <div className="hamburgerMenu" style={{ display: hamDisplay && "flex" }}>
+        <ul className="hamburgerMenu__ul">
           <li>
-            <Link to="/hobbies">
+            <Link
+              to="/hobbies"
+              style={{ textDecoration: "none", color: "black" }}
+            >
               <a>All Hobbies</a>
             </Link>
           </li>
           <li>
-            <Link to="/aboutus">
+            <Link
+              to="/aboutus"
+              style={{ textDecoration: "none", color: "black" }}
+            >
               <a>About Us</a>
             </Link>
           </li>
           <li>
-            <Link to="/sign-in">
+            <Link
+              to="/sign-in"
+              style={{ textDecoration: "none", color: "black" }}
+            >
               <a>Log in</a>
             </Link>
           </li>
         </ul>
         <Link to="/sign-in">
-          <button className="nav__button">Sign Up</button>
+          <button className="hamburgerMenu__button">Sign Up</button>
         </Link>
+        <p onClick={() => setHamDisplay(!hamDisplay)}>Close</p>
       </div>
-
-      <div className="hamburger">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </nav>
+    </>
   );
 };
 
