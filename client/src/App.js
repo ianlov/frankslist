73387bddb866932f6d6039540a9698f88cdb996f
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-11-18 17:11:43 -0800

    layout

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,10 @@ import { useState, useEffect } from "react";
 import SignUpScreen from "./screens/SignUp/SignUp";
 import CreateScreen from "./screens/Create/Create";
 import { verifyUser } from "./services/users";
+import Layout from "./components/Layout/Layout";
 
 const App = () => {
   const [user, setUser] = useState(null);
-
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -21,39 +21,41 @@ const App = () => {
       user ? setUser(user) : setUser(null);
     };
     fetchUser();
-    console.log(user ? true: false)
+    console.log(user ? true : false);
   }, []);
 
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Splash user={user} />
-        </Route>
-        <Route path="/sign-in">
-          <Login setUser={setUser} />
-        </Route>
-        <Route path="/sign-up">
-          <SignUpScreen setUser={setUser} />
-        </Route>
-        <Route path="/hobbies">
-          <Hobbies setUser={setUser} />
-        </Route>
-        <Route path="/hobbies/:id">
-          <Detail setUser={setUser} />
-        </Route>
-        <Route path="/aboutus">
-          <About setUser={setUser} />
-        </Route>
-        <Route path="/newhobby">
-          <CreateScreen setUser={setUser} />
-          {/* {user ? <CreateScreen user={user} /> : <Redirect to="/sign-in" />} */}
-        </Route>
-        <Route path="/myprofile">
-          <Profile />
-          {/* {user ? <Profile user={user} /> : <Redirect to="/sign-up" />} */}
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/">
+            <Splash user={user} />
+          </Route>
+          <Route path="/sign-in">
+            <Login setUser={setUser} />
+          </Route>
+          <Route path="/sign-up">
+            <SignUpScreen setUser={setUser} />
+          </Route>
+          <Route path="/hobbies">
+            <Hobbies setUser={setUser} />
+          </Route>
+          <Route path="/hobbies/:id">
+            <Detail setUser={setUser} />
+          </Route>
+          <Route path="/aboutus">
+            <About setUser={setUser} />
+          </Route>
+          <Route path="/newhobby">
+            <CreateScreen setUser={setUser} />
+            {/* {user ? <CreateScreen user={user} /> : <Redirect to="/sign-in" />} */}
+          </Route>
+          <Route path="/myprofile">
+            <Profile />
+            {/* {user ? <Profile user={user} /> : <Redirect to="/sign-up" />} */}
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 };
