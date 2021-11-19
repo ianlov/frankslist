350430046dee5b8670a41ccdf6cda843b6cc08b3
Vince Abuyuan <rpabuyuan@ucsb.edu>
2021-11-19 07:01:47 -0800

    hobby detail

--- client/src/components/HobbyDetail/Hobbydetail.jsx
+++ client/src/components/HobbyDetail/Hobbydetail.jsx
@@ -1,15 +1,30 @@
 import "./hobbydetail.css";
+import { useParams } from "react-router";
+import { getHobby } from "../../services/hobbies";
+import { useEffect, useState } from "react";
+import { riskScouter } from "./hobbyservices";
 
 const HobbyDetail = () => {
+  const [hobby, setHobby] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    const fetchHobby = async () => {
+      const res = await getHobby(id);
+      console.log(res.price.low);
+      setHobby(res);
+    };
+    fetchHobby();
+  }, []);
   return (
     <>
       <section className="topAbout">
         <div className="topAbout__left">
           <div className="top__leftTop">
-            <h1>Brazilian Jiu Jitsu</h1>
+            <h1>{hobby.name}</h1>
             <div className="top__left__rating">
-              <p>Rate</p>
+              <p>Rating</p>
               <div className="top__left__stars">
+                {hobby.rating}
                 <p>*</p>
                 <p>*</p>
                 <p>*</p>
@@ -21,11 +36,7 @@ const HobbyDetail = () => {
           </div>
           <button className="top__left__button">Save Hobby</button>
         </div>
-        <img
-          className="top__right"
-          src="https://images.unsplash.com/photo-1576149146095-caa19d4de102?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1031&q=80"
-          alt=""
-        />
+        <img className="top__right" src={hobby.img_url} alt="" />
         <div></div>
       </section>
       <section className="bottom">
@@ -41,30 +52,17 @@ const HobbyDetail = () => {
             </ul>
             <ul className="details__ul__right">
               <li style={{ color: "white" }}>+</li>
-              <li>Indoors</li>
-              <li>$100-$200 per Month</li>
-              <li>Risky</li>
+              <li>{hobby.indoors ? "Indoors" : "Outdoors"}</li>
+              <li>
+                ${hobby.price ? hobby.price.low : "0"}-$
+                {hobby.price ? hobby.price.high : "0"} per Month
+              </li>
+              <li>{riskScouter(hobby.risk)}</li>
             </ul>
           </div>
           <div className="bottom__left__down">
             <h4>Description</h4>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-              debitis corrupti perferendis sit illo fugiat, fugit veniam quae
-              sed error deserunt mollitia officia architecto? Perspiciatis
-              itaque facere veritatis expedita temporibus. Ipsa totam ratione
-              amet, consectetur autem nam tempora sit praesentium dignissimos
-              quod dolorem! Dicta delectus sint perspiciatis iste harum incidunt
-              illum praesentium hic optio aspernatur! Possimus sint fugit dolor
-              placeat. Vitae, commodi et necessitatibus itaque, natus possimus
-              dolor modi optio dolorem ratione quasi eius mollitia dolore non
-              assumenda aut nobis facere, voluptatem odit impedit eveniet
-              tempora molestias adipisci saepe? Saepe? Neque quos magni
-              explicabo tempora eos illum aspernatur sint nostrum est error?
-              Deserunt ipsum incidunt sequi provident ab aut, quas inventore sed
-              deleniti quibusdam placeat ad eveniet, voluptatem maxime
-              asperiores.
-            </p>
+            <p>{hobby.description}</p>
           </div>
         </div>
         <div className="bottomDetails__right">
